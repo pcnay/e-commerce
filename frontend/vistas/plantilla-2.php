583eca2a0c28e5c06704b63da33c6ebedc8279d4
soporte <soporte@soporte.pcnay>
2020-01-09 19:53:00 -0800

    Sea grega las carpetas que se utilizaran en el Frontend, ademas se esta diseñando el Encabezado de la pantalla principal

---
+++ frontend/vistas/plantilla-2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<!-- https://wwww.w3schools.com/bootstrap/bootstrap_ref_all_classes.asp 
+Se puede revisar como funciona, y cual es el código.
+https://fontawesome.io/icons
+Maneja los iconos, que se utilizaran en elsistema
+
+-->
+
+<html lang="es">
+  <head>
+    <meta chartset="UTF-8">
+    <!-- Pantallas responsivas  -->
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0, minimun-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="title" content="Tienda Virtual">
+    <meta name= "description" content="Comercio Electronico">
+    <meta name="keyword" content= "comercio electronico ">
+    <title>Tienda Virtual </title>
+    <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+    <script src="js/plugins/jquery.min.js"></script>
+    <script src="js/plugins/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <?php
+      // <!-- CABEZOTE-->  
+      include "modulos/cabezote.php"
+    ?> 
+  </body>
+
+</html>
--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -1,11 +1,123 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta chartset="UTF-8">
-    <title>Tienda Virtual </title>
-  </head>
-  <body>
-    <h1>Tienda Virtual</h1>
-  </body>
+<head>
 
+	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<meta name="title" content="Tienda Virtual">
+
+	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam accusantium enim esse eos officiis sit officia">
+
+	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing, elit, Quisquam, accusantium, enim, esse">
+
+	<title>Tienda Virtual</title>
+
+	<?php
+
+		$icono = ControladorPlantilla::ctrEstiloPlantilla();
+
+		echo '<link rel="icon" href="http://localhost/backend/'.$icono["icono"].'">';
+
+		/*=============================================
+		MANTENER LA RUTA FIJA DEL PROYECTO
+		=============================================*/
+		
+		$url = Ruta::ctrRuta();
+
+	?>
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/bootstrap.min.css">
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plantilla.css">
+
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/cabezote.css">
+
+	<script src="<?php echo $url; ?>vistas/js/plugins/jquery.min.js"></script>
+
+	<script src="<?php echo $url; ?>vistas/js/plugins/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<?php
+
+/*=============================================
+CABEZOTE
+=============================================*/
+
+include "modulos/cabezote.php";
+
+/*=============================================
+CONTENIDO DINÁMICO
+=============================================*/
+
+$rutas = array();
+$ruta = null;
+
+if(isset($_GET["ruta"])){
+
+	$rutas = explode("/", $_GET["ruta"]);
+
+	$item = "ruta";
+	$valor =  $rutas[0];
+
+	/*=============================================
+	URL'S AMIGABLES DE CATEGORÍAS
+	=============================================*/
+
+	$rutaCategorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
+
+	if($rutas[0] == $rutaCategorias["ruta"]){
+
+		$ruta = $rutas[0];
+
+	}
+
+	/*=============================================
+	URL'S AMIGABLES DE SUBCATEGORÍAS
+	=============================================*/
+
+	$rutaSubCategorias = ControladorProductos::ctrMostrarSubCategorias($item, $valor);
+
+	foreach ($rutaSubCategorias as $key => $value) {
+		
+		if($rutas[0] == $value["ruta"]){
+
+			$ruta = $rutas[0];
+
+		}
+
+	}
+
+	/*=============================================
+	LISTA BLANCA DE URL'S AMIGABLES
+	=============================================*/
+
+	if($ruta != null){
+
+		include "modulos/productos.php";
+
+	}else{
+
+		include "modulos/error404.php";
+
+	}
+
+}
+
+?>
+
+<script src="<?php echo $url; ?>vistas/js/cabezote.js"></script>
+<script src="<?php echo $url; ?>vistas/js/plantilla.js"></script>
+
+</body>
 </html>
