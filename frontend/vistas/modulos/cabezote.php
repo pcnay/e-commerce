7e5df11c4af81e1f8da1737c4a4b4af4ebf9ebd3
soporte <soporte@soporte.pcnay>
2020-01-10 12:32:24 -0800

    Se borran carpetas innecesarias del proyecto

--- frontend/vistas/modulos/cabezote.php
+++ frontend/vistas/modulos/cabezote.php
@@ -1,205 +1,104 @@
-<!--=====================================
-TOP
-======================================-->
-
+<!-- TOP -->
 <div class="container-fluid barraSuperior" id="top">
-	
-	<div class="container">
-		
-		<div class="row">
-	
-			<!--=====================================
-			SOCIAL
-			======================================-->
+  <div class="container">
+    <div class="row">
+    <!-- Redes Sociales, definiendo los tamaños de las pantallas -->
+      <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+        <ul>
+          <li>
+            <a href="http://facebook.com" target "_blank">
+            <i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+          </li>
+          <li>
+            <a href="https://www.youtube.com" target "_blank">
+            <i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+          </li>
+          <li>
+            <a href="http://twitter.com" target "_blank">
+            <i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true"></i>
+          </li>
+          <li>
+            <a href="http://google.com" target "_blank">
+            <i class="fa fa-google-plus redSocial googleBlanco" aria-hidden="true"></i>
+          </li>
+          <li>
+            <a href="http://instagram.com" target "_blank">
+            <i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+          </li>
 
-			<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
-				
-				<ul>	
+        </ul>
 
-					<?php
+      </div>
 
-					$social = ControladorPlantilla::ctrEstiloPlantilla();
+      <!-- R3GISTRO --> 
+    <!-- Redes Sociales, definiendo los tamaños de las pantallas -->
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+      <ul>
+        <li><a href="#modalIngreso" data-toggle="modal">Ingresar></a></li>
+        <li>|</li>
+        <li><a href="#modalRegistro" data-toggle="modal">Crear Una Cuenta></a></li>
 
-					$jsonRedesSociales = json_decode($social["redesSociales"],true);		
+      </ul>
 
-					foreach ($jsonRedesSociales as $key => $value) {
+    </div>
 
-						echo '<li>
-								<a href="'.$value["url"].'" target="_blank">
-									<i class="fa '.$value["red"].' redSocial '.$value["estilo"].'" aria-hidden="true"></i>
-								</a>
-							</li>';
-					}
 
-					?>
-			
-				</ul>
+    </div>
+  
+  </div>
 
-			</div>
-
-			<!--=====================================
-			REGISTRO
-			======================================-->
-
-			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
-				
-				<ul>
-					
-					<li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
-					<li>|</li>
-					<li><a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a></li>
-
-				</ul>
-
-			</div>	
-
-		</div>	
-
-	</div>
-
-</div>
-
-<!--=====================================
-HEADER
-======================================-->
+</div> 
+<!-- HEADER -->
+<!-- Las clase que se utilizan en las etiquetas son tomadas de "bootstrap", de apoyo la página w3school.com-->
 
 <header class="container-fluid">
-	
-	<div class="container">
-		
-		<div class="row" id="cabezote">
+  <div class="container">
+    <div class="row" id="cabezote">
+      <!-- LOGOTIPO-->
+      <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+        <a href="#">
+           <img src="http://192.168.1.76/e-commerce/backend/vistas/img/plantilla/logo.png" class= "img-responsive">
+        </a>
 
-			<!--=====================================
-			LOGOTIPO
-			======================================-->
-			
-			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
-				
-				<a href="#">
-						
-					<img src="http://localhost/backend/<?php echo $social["logo"]; ?>" class="img-responsive">
+      </div>
+      <!-- BLOQUE CATEGORIAS Y BUSCADOR -->
+      <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12 backColor" id="btnCategorias">
+        <!-- Boton de categorias -->
+        <p>CATEGORIAS
+          <span class="pull-right">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </span>
+        </p> 
 
-				</a>
-				
-			</div>
+      </div>
+      <!-- Buscador -->
+      <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+        <input type="search" name ="buscar" class="form-control" placeholder="Busca ..."> 
+        <span class = "input-group-btn">
+          <a href="#"
+             <button class = "btn btn-default backColor" type="submit">
+              <i class="fa fa-search"></i> 
+             </button>
 
-			<!--=====================================
-			BLOQUE CATEGORÍAS Y BUSCADOR
-			======================================-->
+          </a>
 
-			<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
-					
-				<!--=====================================
-				BOTÓN CATEGORÍAS
-				======================================-->
+        </span>
 
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
-					
-					<p>CATEGORÍAS
-					
-						<span class="pull-right">
-							<i class="fa fa-bars" aria-hidden="true"></i>
-						</span>
-					
-					</p>
+      </div>
 
-				</div>
+      <!-- Carrito de Compras -->
+      <div class = "col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+        <a href="#">
+          <button class="btn btn-default pull-left backColor">
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          </button>
+        </a>
+        <p>TU CESTA <span class="cantidadCesta"></span><br>USB $ <span class="sumaCesta"></span></p>
+        
+      </div>
+       
+    </div> <!-- <div class="row" id="cabezote"> -->
 
-				<!--=====================================
-				BUSCADOR
-				======================================-->
-				
-				<div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
-					
-					<input type="search" name="buscar" class="form-control" placeholder="Buscar...">	
-
-					<span class="input-group-btn">
-						
-						<a href="#">
-
-							<button class="btn btn-default backColor" type="submit">
-								
-								<i class="fa fa-search"></i>
-
-							</button>
-
-						</a>
-
-					</span>
-
-				</div>
-			
-			</div>
-
-			<!--=====================================
-			CARRITO DE COMPRAS
-			======================================-->
-
-			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
-				
-				<a href="#">
-
-					<button class="btn btn-default pull-left backColor"> 
-						
-						<i class="fa fa-shopping-cart" aria-hidden="true"></i>
-					
-					</button>
-				
-				</a>	
-
-				<p>TU CESTA <span class="cantidadCesta">3</span> <br> USD $ <span class="sumaCesta">20</span></p>	
-
-			</div>
-
-		</div>
-
-		<!--=====================================
-		CATEGORÍAS
-		======================================-->
-
-		<div class="col-xs-12 backColor" id="categorias">
-
-			<?php
-
-				$item = null;
-				$valor = null;
-
-				$categorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
-
-				foreach ($categorias as $key => $value) {
-
-					echo '<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
-							
-							<h4>
-								<a href="'.$value["ruta"].'" class="pixelCategorias">'.$value["categoria"].'</a>
-							</h4>
-							
-							<hr>
-
-							<ul>';
-
-							$item = "id_categoria";
-
-							$valor = $value["id"];
-
-							$subcategorias = ControladorProductos::ctrMostrarSubCategorias($item, $valor);
-							
-							foreach ($subcategorias as $key => $value) {
-									
-									echo '<li><a href="'.$value["ruta"].'" class="pixelSubCategorias">'.$value["subcategoria"].'</a></li>';
-								}	
-								
-							echo '</ul>
-
-						</div>';
-				}
-
-			?>	
-
-		</div>
-
-	</div>
+  </div> <!-- <div class="container"> -->
 
 </header>
-
-
--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -1,123 +1,31 @@
 <!DOCTYPE html>
+
+<!-- https://wwww.w3schools.com/bootstrap/bootstrap_ref_all_classes.asp 
+Se puede revisar como funciona, y cual es el código.
+https://fontawesome.io/icons
+Maneja los iconos, que se utilizaran en elsistema
+
+-->
+
 <html lang="es">
-<head>
+  <head>
+    <meta chartset="UTF-8">
+    <!-- Pantallas responsivas  -->
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0, minimun-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="title" content="Tienda Virtual">
+    <meta name= "description" content="Comercio Electronico">
+    <meta name="keyword" content= "comercio electronico ">
+    <title>Tienda Virtual </title>
+    <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+    <script src="js/plugins/jquery.min.js"></script>
+    <script src="js/plugins/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <?php
+      // <!-- CABEZOTE-->  
+      include "modulos/cabezote.php"
+    ?> 
+  </body>
 
-	<meta charset="UTF-8">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
-	<meta name="title" content="Tienda Virtual">
-
-	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam accusantium enim esse eos officiis sit officia">
-
-	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing, elit, Quisquam, accusantium, enim, esse">
-
-	<title>Tienda Virtual</title>
-
-	<?php
-
-		$icono = ControladorPlantilla::ctrEstiloPlantilla();
-
-		echo '<link rel="icon" href="http://localhost/backend/'.$icono["icono"].'">';
-
-		/*=============================================
-		MANTENER LA RUTA FIJA DEL PROYECTO
-		=============================================*/
-		
-		$url = Ruta::ctrRuta();
-
-	?>
-
-	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/bootstrap.min.css">
-
-	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/font-awesome.min.css">
-
-	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-
-	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
-
-	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plantilla.css">
-
-	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/cabezote.css">
-
-	<script src="<?php echo $url; ?>vistas/js/plugins/jquery.min.js"></script>
-
-	<script src="<?php echo $url; ?>vistas/js/plugins/bootstrap.min.js"></script>
-
-</head>
-
-<body>
-
-<?php
-
-/*=============================================
-CABEZOTE
-=============================================*/
-
-include "modulos/cabezote.php";
-
-/*=============================================
-CONTENIDO DINÁMICO
-=============================================*/
-
-$rutas = array();
-$ruta = null;
-
-if(isset($_GET["ruta"])){
-
-	$rutas = explode("/", $_GET["ruta"]);
-
-	$item = "ruta";
-	$valor =  $rutas[0];
-
-	/*=============================================
-	URL'S AMIGABLES DE CATEGORÍAS
-	=============================================*/
-
-	$rutaCategorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
-
-	if($rutas[0] == $rutaCategorias["ruta"]){
-
-		$ruta = $rutas[0];
-
-	}
-
-	/*=============================================
-	URL'S AMIGABLES DE SUBCATEGORÍAS
-	=============================================*/
-
-	$rutaSubCategorias = ControladorProductos::ctrMostrarSubCategorias($item, $valor);
-
-	foreach ($rutaSubCategorias as $key => $value) {
-		
-		if($rutas[0] == $value["ruta"]){
-
-			$ruta = $rutas[0];
-
-		}
-
-	}
-
-	/*=============================================
-	LISTA BLANCA DE URL'S AMIGABLES
-	=============================================*/
-
-	if($ruta != null){
-
-		include "modulos/productos.php";
-
-	}else{
-
-		include "modulos/error404.php";
-
-	}
-
-}
-
-?>
-
-<script src="<?php echo $url; ?>vistas/js/cabezote.js"></script>
-<script src="<?php echo $url; ?>vistas/js/plantilla.js"></script>
-
-</body>
 </html>
