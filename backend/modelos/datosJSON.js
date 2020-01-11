020b4261c01ecde5e31a411a98aa1d73f44b9549
Ramon <pcnay2003@gmail.com>
2020-01-10 19:33:21 -0800

    Se crea la primer tablay se incia la plantilla dinamica, parte 1

---
+++ backend/modelos/datosJSON.js
@@ -0,0 +1,8 @@
+// Objeto de JSON, para grabar las redes sociales.
+[
+  {"red":"fa-facebook","estilo":"facebookBlanco","url":"https://facebook.com/"}
+  {"red":"fa-youtube","estilo":"youtubeBlanco","url":"https://youtube.com/"}
+  {"red":"fa-twitter","estilo":"twitterBlanco","url":"ittps://twitter.com/"}
+  {"red"i"fa-google-plus","estilo":"googleBlanco","url":"https://google.com/"}
+  {"red":"fa-instgram","estilo":"instgramBlanco","url":"https://instgram.com/"}
+]
--- frontend/index.php
+++ frontend/index.php
@@ -3,6 +3,19 @@
   require_once "controladores/plantilla.controlador.php";
   
   // Vincular la vista del Frontend con el "index.php" principal.
+  /* Cambios permitidos :
+    barraSuperior
+    textoSuperior
+    colorFondo
+    colorTexto
+    (.backColor)
+    Icono de Facebook - redesSociales
+      Escoger red, estilo, url
+      facebook, facebookColor, http://....
+      
+
+  */
+
   $plantilla = new ControladorPlantilla();
   $plantilla->plantilla();
 
